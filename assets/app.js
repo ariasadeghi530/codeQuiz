edc0b4f2923970c4988caaf43537fb29a330174b
Aria Sadeghi <ariasadeghi@gmail.com>
2020-01-25 21:28:09 -0800

    made question answering sequential

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,15 @@
 let wrongStatement = document.createElement('p');
 wrongStatement.className = "text-secondary";
 wrongStatement.textContent = ` Incorrect!`;
+let questionOneCond = true;
+let questionTwoCond = false;
+let questionThreeCond = false;
 
 
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
 
   //start quiz
-  if(event.target.id === 'submit'){
+  if (event.target.id === 'submit') {
     document.getElementById('title').textContent = 'Question 1';
     document.getElementById('prompt').textContent = 'What is a javascript?';
     document.getElementById('btn-wrapper').innerHTML = ` 
@@ -24,18 +27,50 @@ document.addEventListener('click', function(event) {
         </div>
       
     `
-      
-    
-    }
-    console.log(event.target.value);
-  if (event.target.value === '1' || event.target.value === '2' || event.target.value === '3') {
+  }
 
-    document.getElementById('bottom').appendChild(wrongStatement);
+    
+
+    if ((event.target.value === '1' || event.target.value === '2' || event.target.value === '3') && questionOneCond)  {
+      console.log(event.target.value);
+      document.getElementById('bottom').appendChild(wrongStatement);
+
+    }
+
+
+    if ((event.target.value === '4') && questionOneCond) {
+      questionOneCond = false;
+      questionTwoCond = true;
+      document.getElementById('title').textContent = 'Question 2';
+      document.getElementById('prompt').textContent = 'What is not javascript?';
+      document.getElementById('btn-wrapper').innerHTML = ` 
+    <div>
+        <button class="btn btn-outline-primary btn-sm btn-center mb-1" type="submit" value="1">1: meep boop</button>
+        </div>
+    <div>
+        <button class="btn btn-outline-primary btn-sm btn-center mb-1" type="submit" value="2">2: beep moop</button>
+        </div>
+    <div>
+        <button class="btn btn-outline-primary btn-sm btn-center mb-1" type="submit" value="3">3. deet doot</button>
+        </div>
+    <div id="bottom">
+        <button class="btn btn-outline-primary btn-sm btn-center mb-1" type="submit" value="4">4. Peep poo</button>
+        </div>
+      
+    `
+
+    }
+  
+  
+  if ((event.target.value === '1' || event.target.value === '3' || event.target.value === '4') && questionTwoCond) {
+     document.getElementById('bottom').appendChild(wrongStatement);
 
   }
-  if(event.target.value === '4'){
-    document.getElementById('title').textContent = 'Question 2';
-    document.getElementById('prompt').textContent = 'What is not javascript?';
+  if ((event.target.value === '2') && questionTwoCond) {
+    questionTwoCond = false;
+    questionThreeCond = true;
+    document.getElementById('title').textContent = 'Question 3';
+    document.getElementById('prompt').textContent = 'What is not not javascript?';
     document.getElementById('btn-wrapper').innerHTML = ` 
     <div>
         <button class="btn btn-outline-primary btn-sm btn-center mb-1" type="submit" value="1">1: meep boop</button>
@@ -54,3 +89,4 @@ document.addEventListener('click', function(event) {
   }
   
 })
+
