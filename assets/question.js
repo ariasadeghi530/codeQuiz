f5bdc1b1030a5ace589b113b9da8538f39aa4f7f
Aria Sadeghi <ariasadeghi@gmail.com>
2020-01-27 21:22:49 -0800

    added prompt as array of objects

---
+++ assets/question.js
@@ -0,0 +1,22 @@
+
+const questions = 
+[{prompt: 'What is your favorite color?',
+choices: ['Blue','Green','Red','Yellow'],
+answer: 'Blue'
+},
+{ prompt: 'What is my favorite color?',
+  choices: ['Blue', 'Green', 'Red', 'Yellow'],
+  answer: 'Red'
+},
+{ prompt: 'Why are you here?',
+  choices: ['I could ask you the same question', 'Yes?', 'No?', 'my mom made me'],
+  answer: 'Yes?'
+},
+{ prompt: 'Are you a different animal, and the same beast?',
+  choices: [`What the F*** does that mean Kobe Bryant?`, `What are you talking about?`, 'sure', `You're welcome`],
+  answer: `You're welcome`
+},
+{ prompt: 'Press F',
+  choices: ['A', 'D', 'F', 'E'],
+  answer: 'F'
+}];
